5c1f7da6e40c17d1fc59e05ed3af7c0941512452
Deodatus Biche <bichesq@gmail.com>
2026-02-22 17:29:53 +0530

    updated workflow

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,7 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  output: 'standalone',
-  experimental: {},
-  turbopack: {
-    root: __dirname,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
 };
 
 export default nextConfig;
