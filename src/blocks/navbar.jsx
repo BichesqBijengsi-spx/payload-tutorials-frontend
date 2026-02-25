3632ce7c44db57a7fb95da7c8a9e1295eca0652b
Deodatus Biche <bichesq@gmail.com>
2026-02-25 21:02:50 +0530

    modified Hero.jsx

---
+++ src/blocks/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = (logo, navItems, navCTA) => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
