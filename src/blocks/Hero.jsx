a7196b0506c1f785bf6ff9c269f8fac2c76cde00
Deodatus Biche <bichesq@gmail.com>
2026-02-25 20:43:40 +0530

    modified Hero.jsx

--- src/blocks/Hero.jsx
+++ src/blocks/Hero.jsx
@@ -5,6 +5,7 @@ export default function Hero({heading, text, backgroundImage}) {
         <div>
             <h1>{heading}</h1> 
             <p>{text}</p>
+            <img src={backgroundImage} alt="background" />
         </div>
     )
 }
