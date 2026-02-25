89b2ecd5ef1d26094afa472cea50d1a58b813da5
Deodatus Biche <bichesq@gmail.com>
2026-02-26 04:12:19 +0530

    adjustment on hero

--- src/blocks/Hero.jsx
+++ src/blocks/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Hero({heading, text, backgroundImage}) {
+export default function Hero({title, text, backgroundImage}) {
     return (
         <div>
-            <h1>{heading}</h1> 
+            <h1>{title}</h1> 
             <p>{text}</p>
             {backgroundImage?.url && (
                 <img src={backgroundImage.url} alt={backgroundImage.alt || 'background'} />
